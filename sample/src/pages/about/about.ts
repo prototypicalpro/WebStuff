e35aea29bef3e5432791b526cfa3ea942f8e1e1d
Noah <noah@koontzs.com>
2017-05-19 20:43:33 -0700

    some theming stuff, this won't actually be that hard (lol jk)

--- sample/src/pages/about/about.ts
+++ sample/src/pages/about/about.ts
@@ -8,8 +8,15 @@ import { URLUtil } from '../../lib/URLUtil.service';
 })
 export class AboutPage {
   tab: URLUtil;
+  themePick: string;
 
   constructor(public navCtrl: NavController, private opener: URLUtil) {
     this.tab = opener;
+    this.themePick = 'this-theme';
+  }
+
+  toggleTheme(){
+    if(this.themePick == 'this-theme') this.themePick = 'that-theme';
+    else this.themePick = 'this-theme';
   }
 }
