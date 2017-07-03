5d68d32b3eeebf39b6bea3b996a077acb79beb45
Noah <noah@koontzs.com>
2017-07-02 20:33:26 -0700

    rewrote most of the app in raw typescript, launch times are legendary

---
+++ HereWeGo/HereWeGo/scripts/HTMLMap.ts
@@ -0,0 +1,32 @@
+﻿/*
+ * File which will contain constants for all the identifiers and classes
+ * used in the HTML, so that I have a central place to find all these stupid constants
+ * freakin android can't handle a templating engine
+ */
+
+export const HTMLMap = {
+    class: "app",
+    background: {
+        class: "background",
+        image: "image",
+        gradient: "shader"
+    },
+    topContent: {
+        class: "top",
+        container: "cont",
+        text: "text",
+        upperText: "upText",
+        lowerText: "lowText"
+    },
+    bottomContent: {
+        class: "bottom",
+        container: "cont",
+        bar: "centerBar",
+        button: "fab",
+        timeText: "timeText",
+        gradient: "shader"
+    },
+    font: {
+        WHSFont: "GeometricSlabSerif703Bold"
+    }
+}
