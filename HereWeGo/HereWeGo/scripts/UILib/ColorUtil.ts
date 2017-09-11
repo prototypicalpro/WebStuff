faead4ee63c3ddb984e753240aa90c06b76cfbea
Noah <noah@koontzs.com>
2017-09-10 21:13:10 -0700

    forgot this bugger

---
+++ HereWeGo/HereWeGo/scripts/UILib/ColorUtil.ts
@@ -0,0 +1,18 @@
+﻿/**
+ * Some Color functions I stole from
+ * https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+ */
+
+namespace ColorUtil {
+    export const shadeColors = (color: string, percent: number): string => {
+        var f = parseInt(color.slice(1), 16), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent, R = f >> 16, G = f >> 8 & 0x00FF, B = f & 0x0000FF;
+        return "#" + (0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1);
+    };
+
+    export const blendColors = (c0: string, c1: string, p: number): string => {
+        var f = parseInt(c0.slice(1), 16), t = parseInt(c1.slice(1), 16), R1 = f >> 16, G1 = f >> 8 & 0x00FF, B1 = f & 0x0000FF, R2 = t >> 16, G2 = t >> 8 & 0x00FF, B2 = t & 0x0000FF;
+        return "#" + (0x1000000 + (Math.round((R2 - R1) * p) + R1) * 0x10000 + (Math.round((G2 - G1) * p) + G1) * 0x100 + (Math.round((B2 - B1) * p) + B1)).toString(16).slice(1);
+    }
+}
+
+export = ColorUtil;
