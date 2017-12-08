c49a0d7ed34c034501218c95e8dcbc1e8ab4364d
IsMac <noah@koontzs.com>
2017-12-07 19:25:47 -0800

    added cheap build script

---
+++ HereWeGo/HereWeGo/build-and-run-ios.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Building..."
+cordova build ios
+
+echo Build Finished!
+echo Booting Simulator and Installing App!
+xcrun simctl boot CD8F73AD-C756-493D-B249-31689350608D
+open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.appj
+xcrun simctl install CD8F73AD-C756-493D-B249-31689350608D ./platforms/ios/build/emulator/WilsonAppAlpha.app
+echo "Launching..."
+xcrun simctl launch CD8F73AD-C756-493D-B249-31689350608D com.pileofwires.wilsonappalpha
