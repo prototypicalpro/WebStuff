a73817dd3306fc6ba5dd38024dcd48fc3c31981e
IsMac <noah@koontzs.com>
2017-12-20 12:56:45 -0800

    ios fixes again

--- HereWeGo/HereWeGo/build-and-run-ios.sh
+++ HereWeGo/HereWeGo/build-and-run-ios.sh
@@ -5,8 +5,9 @@ cordova build ios
 
 echo Build Finished!
 echo Booting Simulator and Installing App!
-xcrun simctl boot CD8F73AD-C756-493D-B249-31689350608D
+xcrun simctl boot E283519E-C91E-4322-A017-F30C9B4234AD
 open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
-xcrun simctl install CD8F73AD-C756-493D-B249-31689350608D ./platforms/ios/build/emulator/WilsonAppAlpha.app
+xcrun simctl install E283519E-C91E-4322-A017-F30C9B4234AD ./platforms/ios/build/emulator/Wilson\ App\ Alpha.app
 echo "Launching..."
-xcrun simctl launch CD8F73AD-C756-493D-B249-31689350608D com.pileofwires.wilsonappalpha
+xcrun simctl launch E283519E-C91E-4322-A017-F30C9B4234AD com.pileofwires.wilsonappalpha
+exit 0
